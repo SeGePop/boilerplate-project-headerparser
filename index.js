dd6f87b7aa472e241f5a5f1ca94891f9d0046d78
Pop Sergiu <sergiugeorgepop@gmail.com>
2024-04-03 19:11:05 +0300

    fix/finished project

--- index.js
+++ index.js
@@ -24,8 +24,10 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
-app.get("/api", (req,res) => {
-  res.json({"ipaddress":"::ffff:159.20.14.100","language":"en-US,en;q=0.5",
+app.get("/api/whoami", (req,res) => {
+  res.json({
+  "ipaddress":"::ffff:159.20.14.100",
+  "language":"en-US,en;q=0.5",
   "software":"Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:50.0) Gecko/20100101 Firefox/50.0"})
 })
 
